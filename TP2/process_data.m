491b08731721e50654489bab3dfc9d19dd932c53
Felix Prevost <felix.prevost@polymtl.ca>
2016-03-17 16:26:16 -0400

    Matlab script now computes the average time and revenu

--- TP2/process_data.m
+++ TP2/process_data.m
@@ -4,7 +4,6 @@ local = 2;
 
 %Useful indices
 nbLocations = 2;
-maxRevenu = 3;
 maxQuantity = 3;
 supplierQuantity = 4;
 time = 5;
@@ -16,7 +15,16 @@ dynamicData = data(data(:,1) == dynamic,:);
 localData = data(data(:,1) == local,:);
 
 % Greedy algorithm
+disp('Greedy')
+R = greedyData(:, revenu);
 Y = greedyData(:,time);
+
+averageRevenu = sum(R) / size(R,1);
+averageTime = sum(Y) / size(Y,1);
+
+fprintf('Average revenu : %f\n', averageRevenu);
+fprintf('Average time : %fs\n\n', averageTime);
+
 hyp = greedyData(:, nbLocations) .* greedyData(:, supplierQuantity) ./ greedyData(:, maxQuantity);
 [c, r2] = regression(hyp, Y);
 
@@ -31,12 +39,45 @@ plot(hyp, c(1)*hyp + c(2));
 hold off;
 
 % Dynamic programming algorithm
+disp('Dynamic')
+R = dynamicData(:, revenu);
 Y = dynamicData(:,time);
 hyp = dynamicData(:,nbLocations) .* dynamicData(:,supplierQuantity);
 [c, r2] = regression(hyp, Y);
 
+averageRevenu = sum(R) / size(R,1);
+averageTime = sum(Y) / size(Y,1);
+
+fprintf('Average revenu : %f\n', averageRevenu);
+fprintf('Average time : %fs\n\n', averageTime);
+
 figure();
-titre = sprintf('Test des constantes pour confirmer l''hypothese sur l''algorithme\nde programmation dynamique probabiliste (R2 = %f)', r2);
+titre = sprintf('Test des constantes pour confirmer l''hypothese sur l''algorithme\nde programmation dynamique (R2 = %f)', r2);
+title(titre);
+xlabel('Hypothèse');
+ylabel('Temps d''exécution (s)');
+hold on;
+plot(hyp, Y, 'x');
+plot(hyp, c(1)*hyp + c(2));
+hold off;
+
+% Local optimizations algorithm
+disp('Local')
+R = localData(:,revenu);
+Y = localData(:,time);
+P = localData(:,supplierQuantity) ./ localData(:, maxQuantity);
+N = localData(:,nbLocations);
+hyp = P .* N .* N;
+[c, r2] = regression(hyp, Y);
+
+averageRevenu = sum(R) / size(R,1);
+averageTime = sum(Y) / size(Y,1);
+
+fprintf('Average revenu : %f\n', averageRevenu);
+fprintf('Average time : %fs\n', averageTime);
+
+figure();
+titre = sprintf('Test des constantes pour confirmer l''hypothese sur l''algorithme\nd''optimisations locales (R2 = %f)', r2);
 title(titre);
 xlabel('Hypothèse');
 ylabel('Temps d''exécution (s)');
